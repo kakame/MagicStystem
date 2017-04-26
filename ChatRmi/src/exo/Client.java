63c56e1a38c6d892b9928c2f34b8c645b86e9d57
Sofiane Adjiri <sofiane.adjiri@a-018651.ups.u-psud.fr>
2017-04-26 16:34:12 +0200

    tt

--- ChatRmi/src/exo/Client.java
+++ ChatRmi/src/exo/Client.java
@@ -25,15 +25,21 @@ public class Client implements RemoteClient {
 			System.err.println("Usage: java " + Server.class.getName() + " nshost nsport name");
 			System.exit(1);
 		}
-
+		
+		//se connecter au register
 		Registry registry = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+		// chopper le stub du serveur 
 		RemoteChat test = (RemoteChat) registry.lookup(args[2]); 
+		
 		Scanner s = new Scanner (System.in);
 		System.out.println("Enter name : ");
 		
 		//attention c'est le stub qu'il faut envoyer 
+		//on cree une instance du client 
 		RemoteClient c = new Client (s.nextLine().trim());
+		//on cree un stub qui représente l'instance du client et que l'on va ajouter dans la liste de gestion des clients du serveur
 		RemoteClient stub = (RemoteClient) UnicastRemoteObject.exportObject(c, 0); 
+		//on utilise le stub du serveur pour rajouter le stub du client dans la liste du serveur 
 		test.addClient(stub);
 		
 		
